25e54c3d1b2e06c9e1cd5621d7187578a1270084
Arpan25AK <akarpan2005@gmail.com>
2026-05-16 16:46:23 +0530

    structure push,intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+      <div className="min-h-screen bg-[#0d1117]">
+        <h1 style={{color: "navajowhite"}}>DevFlow</h1>
+      </div>
+  )
+}
+
+
+export default App
