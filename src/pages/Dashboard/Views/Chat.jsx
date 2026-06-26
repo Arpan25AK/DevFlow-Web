2b96e41fc2dbd0eeb8bd6c31ca5c3966de51ce2e
Arpan25AK <akarpan2005@gmail.com>
2026-06-26 17:20:45 +0530

    setup the back ground for the chat page need some planning to route out the design for the chat service

--- src/pages/Dashboard/Views/Chat.jsx
+++ src/pages/Dashboard/Views/Chat.jsx
@@ -1,5 +1,6 @@
 import {useNavigate} from "react-router-dom";
 import styles from "./Chat.module.css"
+import logo from "../../../assets/git.jpg";
 
 function Chat(){
     const navigate = useNavigate()
@@ -7,7 +8,28 @@ function Chat(){
     return(
 
         <div className={styles.bg}>
-            <h1 style={{color : 'whitesmoke'}}>hello</h1>
+
+            <div className={styles.patternBg}>
+                <svg className={styles.cubeSvg} xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <pattern id="cubes" x="0" y="0" width="80" height="80" patternUnits="userSpaceOnUse">
+                            <polygon points="40,5 75,25 75,55 40,75 5,55 5,25" fill="none" stroke="#00d4d433" strokeWidth="1"/>
+                        </pattern>
+                    </defs>
+                    <rect width="100%" height="100%" fill="url(#cubes)"/>
+                </svg>
+            </div>
+
+
+            <div className={styles.pageContent}>
+
+                <div className={styles.navbar}>
+                    <div className={styles.logogrp}>
+                        <img className={styles.logoimg} src={logo} alt="logo" />
+                        <button className={styles.logoText} onClick={() => navigate('/')}>DevFlow</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
